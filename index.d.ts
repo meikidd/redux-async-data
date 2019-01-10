1bfbbd7d06e9e2326ff05987e1b563b66c1a0eb0
meikidd <meikidd@gmail.com>
2019-01-10 17:37:10 +0800

    update type definition index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -16,18 +16,14 @@ declare module 'redux-async-data' {
     hasMore?: boolean; // hasMore is used for pagination 
   };
 
-  export function createAsyncData<T>(
-    data: T,
-    enablePaging?: boolean
-  ): AsyncData<T>;
+  export function createAsyncData<T>(data: T, enablePaging?: boolean): AsyncData<T>;
+  export function createAsyncAction(type: string, status: Status, payload?: any): AnyAction;
 
-  export function   createAsyncAction(
-    type: string,
-    status: Status,
-    payload?: any
-  ): AnyAction;
+  export function pending(type: string,payload?: any): AnyAction;
+  export function succeeded(type: string,payload?: any): AnyAction;
+  export function failed(type: string,payload?: any): AnyAction;
 
-  export function   createAsyncType(type: string, status: Status): string;
+  export function createAsyncType(type: string, status: Status): string;
 
   export function updateAsyncData<T>(
     asyncData: AsyncData<T>,
@@ -35,10 +31,10 @@ declare module 'redux-async-data' {
     reducer?: Reducer
   ): AsyncData<T>
 
-  export function parseAsyncType(
-    type: string
-  ): { type: string; status: Status | null }
+  export function parseAsyncType(type: string): { type: string; status: Status | null }
 
   export function isPending(asyncData: AsyncData<any>): boolean;
+  export function isFailed(asyncData: AsyncData<any>): boolean;
+  export function isSucceeded(asyncData: AsyncData<any>): boolean;
 
 }
