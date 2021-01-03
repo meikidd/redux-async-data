0b63a934cee4a5722662320fca5d7d689c85c901
meikidd <meikidd@gmail.com>
2021-01-03 20:32:23 +0800

    export umd module using babel

--- dist/AsyncView.js
+++ dist/AsyncView.js
@@ -1,30 +1,60 @@
-import * as React from 'react';
-import { isInit, isPending, isSucceeded, isFailed } from './index';
-var isEmptyArray = function (data) {
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(["exports", "react", "./index"], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports, require("react"), require("./index"));
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports, global.react, global.index);
+    global.AsyncView = mod.exports;
+  }
+})(typeof globalThis !== "undefined" ? globalThis : typeof self !== "undefined" ? self : this, function (_exports, React, _index) {
+  "use strict";
+
+  Object.defineProperty(_exports, "__esModule", {
+    value: true
+  });
+  _exports.AsyncView = AsyncView;
+  React = _interopRequireWildcard(React);
+
+  function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
+
+  function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+  const isEmptyArray = function (data) {
     return data.length === 0;
-};
-var isEmptyObject = function (data) {
+  };
+
+  const isEmptyObject = function (data) {
     return Object.keys(data).length === 0;
-};
-export function AsyncView(props) {
-    var data = props.data, init = props.init, pending = props.pending, failed = props.failed, empty = props.empty, succeeded = props.succeeded, checkEmpty = props.checkEmpty;
-    var result = data.data;
-    var isEmpty = !result ||
-        isEmptyArray(result) ||
-        isEmptyObject(result) ||
-        (!!checkEmpty && checkEmpty(result));
-    var content = succeeded;
-    if (isInit(data) && !!init) {
-        content = init;
+  };
+
+  function AsyncView(props) {
+    const {
+      data,
+      init,
+      pending,
+      failed,
+      empty,
+      succeeded,
+      checkEmpty
+    } = props;
+    const result = data.data;
+    const isEmpty = !result || isEmptyArray(result) || isEmptyObject(result) || !!checkEmpty && checkEmpty(result);
+    let content = succeeded;
+
+    if ((0, _index.isInit)(data) && !!init) {
+      content = init;
+    } else if ((0, _index.isPending)(data) && !!pending) {
+      content = pending;
+    } else if ((0, _index.isFailed)(data) && !!failed) {
+      content = failed;
+    } else if ((0, _index.isSucceeded)(data) && isEmpty && !!empty) {
+      content = empty;
     }
-    else if (isPending(data) && !!pending) {
-        content = pending;
-    }
-    else if (isFailed(data) && !!failed) {
-        content = failed;
-    }
-    else if (isSucceeded(data) && isEmpty && !!empty) {
-        content = empty;
-    }
-    return React.createElement(React.Fragment, null, content);
-}
+
+    return <React.Fragment>{content}</React.Fragment>;
+  }
+});
--- dist/index.js
+++ dist/index.js
@@ -1,128 +1,216 @@
-/*
-
-Status flow:
-
-         INIT
-          |
-       PENDING
-        /   \
-SUCCEEDED   FAILED
-
-*/
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(["exports", "./AsyncView"], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports, require("./AsyncView"));
+  } else {
+    var mod = {
+      exports: {}
     };
-    return __assign.apply(this, arguments);
-};
-export var Status;
-(function (Status) {
+    factory(mod.exports, global.AsyncView);
+    global.index = mod.exports;
+  }
+})(typeof globalThis !== "undefined" ? globalThis : typeof self !== "undefined" ? self : this, function (_exports, _AsyncView) {
+  "use strict";
+
+  Object.defineProperty(_exports, "__esModule", {
+    value: true
+  });
+  var _exportNames = {
+    Status: true,
+    createAsyncData: true,
+    createAsyncType: true,
+    createAsyncAction: true,
+    pending: true,
+    succeeded: true,
+    failed: true,
+    noMore: true,
+    updateAsyncData: true,
+    parseAsyncType: true,
+    isInit: true,
+    isPending: true,
+    isFailed: true,
+    isSucceeded: true,
+    isFinished: true
+  };
+  _exports.createAsyncData = createAsyncData;
+  _exports.createAsyncType = createAsyncType;
+  _exports.createAsyncAction = createAsyncAction;
+  _exports.pending = pending;
+  _exports.succeeded = succeeded;
+  _exports.failed = failed;
+  _exports.noMore = noMore;
+  _exports.updateAsyncData = updateAsyncData;
+  _exports.parseAsyncType = parseAsyncType;
+  _exports.isInit = isInit;
+  _exports.isPending = isPending;
+  _exports.isFailed = isFailed;
+  _exports.isSucceeded = isSucceeded;
+  _exports.isFinished = isFinished;
+  _exports.Status = void 0;
+  Object.keys(_AsyncView).forEach(function (key) {
+    if (key === "default" || key === "__esModule") return;
+    if (Object.prototype.hasOwnProperty.call(_exportNames, key)) return;
+    if (key in _exports && _exports[key] === _AsyncView[key]) return;
+    Object.defineProperty(_exports, key, {
+      enumerable: true,
+      get: function () {
+        return _AsyncView[key];
+      }
+    });
+  });
+
+  /*
+  
+  Status flow:
+  
+           INIT
+            |
+         PENDING
+          /   \
+  SUCCEEDED   FAILED
+  
+  */
+  let Status;
+  _exports.Status = Status;
+
+  (function (Status) {
     Status["INIT"] = "INIT";
     Status["PENDING"] = "PENDING";
     Status["SUCCEEDED"] = "SUCCEEDED";
     Status["FAILED"] = "FAILED";
     Status["NO_MORE"] = "NO_MORE";
-})(Status || (Status = {}));
-export function createAsyncData(data, enablePaging) {
+  })(Status || (_exports.Status = Status = {}));
+
+  function createAsyncData(data, enablePaging) {
     return {
-        status: Status.INIT,
-        data: data,
-        hasMore: enablePaging
+      status: Status.INIT,
+      data: data,
+      hasMore: enablePaging
     };
-}
-export function createAsyncType(type, status) {
+  }
+
+  function createAsyncType(type, status) {
     return type + '.' + status;
-}
-export function createAsyncAction(type, status, payload) {
-    return { type: createAsyncType(type, status), payload: payload, isAsync: true };
-}
-export function pending(type, payload) {
+  }
+
+  function createAsyncAction(type, status, payload) {
     return {
-        type: createAsyncType(type, Status.PENDING),
-        payload: payload,
-        isAsync: true
+      type: createAsyncType(type, status),
+      payload,
+      isAsync: true
     };
-}
-export function succeeded(type, payload) {
+  }
+
+  function pending(type, payload) {
     return {
-        type: createAsyncType(type, Status.SUCCEEDED),
-        payload: payload,
-        isAsync: true
+      type: createAsyncType(type, Status.PENDING),
+      payload,
+      isAsync: true
     };
-}
-export function failed(type, payload) {
-    return { type: createAsyncType(type, Status.FAILED), payload: payload, isAsync: true };
-}
-export function noMore(type, payload) {
+  }
+
+  function succeeded(type, payload) {
     return {
-        type: createAsyncType(type, Status.NO_MORE),
-        payload: payload,
-        isAsync: true
+      type: createAsyncType(type, Status.SUCCEEDED),
+      payload,
+      isAsync: true
     };
-}
-export function updateAsyncData(asyncData, action, reducer) {
-    var status = parseAsyncType(action.type).status;
+  }
+
+  function failed(type, payload) {
+    return {
+      type: createAsyncType(type, Status.FAILED),
+      payload,
+      isAsync: true
+    };
+  }
+
+  function noMore(type, payload) {
+    return {
+      type: createAsyncType(type, Status.NO_MORE),
+      payload,
+      isAsync: true
+    };
+  }
+
+  function updateAsyncData(asyncData, action, reducer) {
+    const {
+      status
+    } = parseAsyncType(action.type);
+
     if (status === Status.FAILED) {
-        return __assign({}, asyncData, { status: Status.FAILED, error: action.payload });
+      return { ...asyncData,
+        status: Status.FAILED,
+        error: action.payload
+      };
+    } else if (status === Status.NO_MORE) {
+      return { ...asyncData,
+        hasMore: false
+      };
+    } else if (status === Status.PENDING) {
+      return { ...asyncData,
+        status: Status.PENDING
+      };
+    } else if (status === Status.SUCCEEDED) {
+      return { ...asyncData,
+        status: Status.SUCCEEDED,
+        data: !!reducer ? reducer(asyncData.data, action) : action.payload
+      };
+    } else {
+      return asyncData;
     }
-    else if (status === Status.NO_MORE) {
-        return __assign({}, asyncData, { hasMore: false });
-    }
-    else if (status === Status.PENDING) {
-        return __assign({}, asyncData, { status: Status.PENDING });
-    }
-    else if (status === Status.SUCCEEDED) {
-        return __assign({}, asyncData, { status: Status.SUCCEEDED, data: !!reducer ? reducer(asyncData.data, action) : action.payload });
-    }
-    else {
-        return asyncData;
-    }
-}
-// parseAsyncType('FETCH_TODO_LIST.PENDING') -> { type: 'FETCH_TODO_LIST', status: 'PENDING' }
-// parseAsyncType('SYNC_ACTION_TYPE') -> { type: 'SYNC_ACTION_TYPE', status: null }
-export function parseAsyncType(type) {
-    var matches = type.match(/^(\S+)\.(\w+$)/);
+  } // parseAsyncType('FETCH_TODO_LIST.PENDING') -> { type: 'FETCH_TODO_LIST', status: 'PENDING' }
+  // parseAsyncType('SYNC_ACTION_TYPE') -> { type: 'SYNC_ACTION_TYPE', status: null }
+
+
+  function parseAsyncType(type) {
+    const matches = type.match(/^(\S+)\.(\w+$)/);
+
     if (matches && matches.length >= 3) {
-        var status_1 = null;
-        if (Status.FAILED === matches[2]) {
-            status_1 = Status.FAILED;
-        }
-        else if (Status.INIT === matches[2]) {
-            status_1 = Status.INIT;
-        }
-        else if (Status.NO_MORE === matches[2]) {
-            status_1 = Status.NO_MORE;
-        }
-        else if (Status.PENDING === matches[2]) {
-            status_1 = Status.PENDING;
-        }
-        else if (Status.SUCCEEDED === matches[2]) {
-            status_1 = Status.SUCCEEDED;
-        }
-        return { type: matches[1], status: status_1 };
+      let status = null;
+
+      if (Status.FAILED === matches[2]) {
+        status = Status.FAILED;
+      } else if (Status.INIT === matches[2]) {
+        status = Status.INIT;
+      } else if (Status.NO_MORE === matches[2]) {
+        status = Status.NO_MORE;
+      } else if (Status.PENDING === matches[2]) {
+        status = Status.PENDING;
+      } else if (Status.SUCCEEDED === matches[2]) {
+        status = Status.SUCCEEDED;
+      }
+
+      return {
+        type: matches[1],
+        status
+      };
+    } else {
+      return {
+        type,
+        status: null
+      };
     }
-    else {
-        return { type: type, status: null };
-    }
-}
-export function isInit(asyncData) {
+  }
+
+  function isInit(asyncData) {
     return asyncData.status === Status.INIT;
-}
-export function isPending(asyncData) {
+  }
+
+  function isPending(asyncData) {
     return asyncData.status === Status.PENDING;
-}
-export function isFailed(asyncData) {
+  }
+
+  function isFailed(asyncData) {
     return asyncData.status === Status.FAILED;
-}
-export function isSucceeded(asyncData) {
+  }
+
+  function isSucceeded(asyncData) {
     return asyncData.status === Status.SUCCEEDED;
-}
-export function isFinished(asyncData) {
+  }
+
+  function isFinished(asyncData) {
     return isFailed(asyncData) || isSucceeded(asyncData);
-}
-export * from './AsyncView';
+  }
+});
